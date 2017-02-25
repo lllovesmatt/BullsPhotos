ff46c694463b4f7a5c735fa1997f15ea33b1aa9f
Matthew Ramsden <ramsden.matthew@gmail.com>
2017-02-24 19:52:07 -0600

    Add Files

---
+++ BullsPhotos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullsPhotos
+//
+//  Created by Matthew Ramsden on 2/24/17.
+//  Copyright © 2017 Matthew Ramsden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
