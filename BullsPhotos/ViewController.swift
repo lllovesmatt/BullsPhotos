362009e8ca5e4bb43345b4490828f34a834c2022
Matthew Ramsden <ramsden.matthew@gmail.com>
2017-02-25 12:06:16 -0600

    Image and Title working correctly

--- BullsPhotos/ViewController.swift
+++ BullsPhotos/ViewController.swift
@@ -22,7 +22,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     // MARK: Actions
@@ -49,9 +48,105 @@ class ViewController: UIViewController {
 
     private func getImageFromFlickr() {
         
-        let urlString = "\(APIConstants.Flickr.APIBaseURL)?\(APIConstants.FlickrParameterKeys.Method)=\(APIConstants.FlickrParameterValues.GalleryPhotosMethod)&\(APIConstants.FlickrParameterKeys.APIKey)=\(APIConstants.FlickrParameterValues.APIKey)&\(APIConstants.FlickrParameterKeys.GalleryID)=\(APIConstants.FlickrParameterValues.GalleryID)&\(APIConstants.FlickrParameterKeys.Extras)=\(APIConstants.FlickrParameterValues.MediumURL)&\(APIConstants.FlickrParameterKeys.Format)=\(APIConstants.FlickrParameterValues.ResponseFormat)&\(APIConstants.FlickrParameterKeys.NoJSONCallback)=\(APIConstants.FlickrParameterValues.DisableJSONCallback)"
-        let url = URL(string: urlString)!
-        print(url)
+//        let urlString = "\(APIConstants.Flickr.APIBaseURL)?\(APIConstants.FlickrParameterKeys.Method)=\(APIConstants.FlickrParameterValues.GalleryPhotosMethod)&\(APIConstants.FlickrParameterKeys.APIKey)=\(APIConstants.FlickrParameterValues.APIKey)&\(APIConstants.FlickrParameterKeys.GalleryID)=\(APIConstants.FlickrParameterValues.GalleryID)&\(APIConstants.FlickrParameterKeys.Extras)=\(APIConstants.FlickrParameterValues.MediumURL)&\(APIConstants.FlickrParameterKeys.Format)=\(APIConstants.FlickrParameterValues.ResponseFormat)&\(APIConstants.FlickrParameterKeys.NoJSONCallback)=\(APIConstants.FlickrParameterValues.DisableJSONCallback)"
+//        let url = URL(string: urlString)!
+//        print(url)
+//        
+        let methodParameters = [
+            APIConstants.FlickrParameterKeys.Method:APIConstants.FlickrParameterValues.GalleryPhotosMethod,
+            APIConstants.FlickrParameterKeys.APIKey:APIConstants.FlickrParameterValues.APIKey,
+            APIConstants.FlickrParameterKeys.GalleryID:APIConstants.FlickrParameterValues.GalleryID,
+            APIConstants.FlickrParameterKeys.Extras:APIConstants.FlickrParameterValues.MediumURL,
+            APIConstants.FlickrParameterKeys.Format:APIConstants.FlickrParameterValues.ResponseFormat,
+            APIConstants.FlickrParameterKeys.NoJSONCallback:APIConstants.FlickrParameterValues.DisableJSONCallback
+        
+        ]
+        
+        let urlString = APIConstants.Flickr.APIBaseURL + escapedParameters(parameters: methodParameters as [String : AnyObject])
+        
+        let url = NSURL(string: urlString)!
+        
+        let request = NSMutableURLRequest(url: url as URL)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
+            
+            func displayError(error: String) {
+                print(error)
+                print("URL at time of error: \(url)")
+                performUIUpdatesOnMain {
+                    self.setUIEnabled(true)
+                }
+            }
+            
+            if error == nil {
+                if let data = data {
+                    let parsedResult: Any!
+                    do {
+                    parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                    } catch {
+                        displayError(error: "Could not Parse the JSON: \(data)")
+                        return
+                    }
+                    
+                    //print(parsedResult)
+                
+                    let parsedResultAnyObject: AnyObject = parsedResult as AnyObject
+                    if let photosDictionary = parsedResultAnyObject[APIConstants.FlickrResponseKeys.Photos] as? [String: AnyObject], let photoArray = photosDictionary[APIConstants.FlickrResponseKeys.Photo] as? [[String: AnyObject]] {
+                        //print(photosDictionary)
+                        //print(photoArray[5])
+                        
+                        let randomIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+                        let photoDictionary = photoArray[randomIndex]
+                        
+                        if let urlImageString = photoDictionary[APIConstants.FlickrResponseKeys.MediumURL] as? String, let urlImageTitle = photoDictionary[APIConstants.FlickrResponseKeys.Title] as? String {
+//                            print(urlImageString)
+//                            print(urlImageTitle)
+                            
+                            let imageURL = URL(string: urlImageString)
+                            if let imageData = NSData(contentsOf: imageURL!) {
+                                
+                                performUIUpdatesOnMain {
+                                    self.photoImageView.image = UIImage(data: imageData as Data)
+                                    self.photoTitleLabel.text = urlImageTitle
+                                    self.setUIEnabled(true)
+                                }
+                                
+                                
+                                
+                            }
+                        }
+                        
+
+                    }
+                
+                }
+            }
+            
+            
+        }
+        
+        task.resume()
+        
+
+//        let session = URLSession.shared
+//        
+//        let dataTask = session.dataTask(with: request as URLRequest) {data,response,error in
+//            let httpResponse = response as? HTTPURLResponse
+//            
+//            if (error != nil) {
+//                print(error!)
+//            } else {
+//                print(httpResponse!)
+//            }
+//            
+//            DispatchQueue.main.async {
+//                //Update your UI here
+//            }
+//            
+//        }
+//        dataTask.resume()
+        
+        
         
         
     }
